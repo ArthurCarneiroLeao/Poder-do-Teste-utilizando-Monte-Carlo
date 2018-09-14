26875be882de910823203efbee0e456f5afd9c60
ArthurCarneiroLeao <43119466+ArthurCarneiroLeao@users.noreply.github.com>
2018-09-13 21:48:31 -0300

    Update Poder_Cauchy.R

--- Poder_Cauchy.R
+++ Poder_Cauchy.R
@@ -4,9 +4,9 @@
   mu0 <- 1/2       
   mu.test <- c(0.1,0.15,0.2,0.25,0.3,0.4,.5,1,1.5,2,2.5,3,3.5,4,4.5,5)   
   M <- length(mu.test)              
-  power <- numeric(M)               
+  poder <- numeric(M)               
   nobs <- c(20, 40, 50, 100)        
-  power_nobs <- matrix(0,length(nobs),M)  
+  poder_nobs <- matrix(0,length(nobs),M)  
   c <- 1
   for(j in nobs) {
     for (i in 1:M) {
@@ -15,27 +15,27 @@
         x <- rcauchy(j, mu, 1)
         test_t <- t.test(x, alternative = "greater", mu = mu0)
         test_t$p.value})
-      power[i] <- mean(p_value <= 0.05)
+      poder[i] <- mean(p_value <= 0.05)
     }
-    power_nobs[c,] <- power
+    poder_nobs[c,] <- poder
     c = c+1
   }
   
-  #Gráficos com n diferentes
+  #GrÃ¡ficos com n diferentes
   x11()
   par(mfrow=c(2,2))
-  plot(mu.test, power_nobs[1,], type = "l", xlab = bquote(theta), ylab = "Poder", main = "n = 20")
+  plot(mu.test, poder_nobs[1,], type = "l", xlab = bquote(theta), ylab = "Poder", main = "n = 20")
   abline(v = mu0, lty = 1)
   abline(h = .05, lty = 1)
   
-  plot(mu.test, power_nobs[2,], type = "l", xlab = bquote(theta), ylab = "Poder", main = "n = 40")
+  plot(mu.test, poder_nobs[2,], type = "l", xlab = bquote(theta), ylab = "Poder", main = "n = 40")
   abline(v = mu0, lty = 1)
   abline(h = .05, lty = 1)
   
-  plot(mu.test, power_nobs[3,], type = "l", xlab = bquote(theta), ylab = "Poder", main = "n = 50")
+  plot(mu.test, poder_nobs[3,], type = "l", xlab = bquote(theta), ylab = "Poder", main = "n = 50")
   abline(v = mu0, lty = 1)
   abline(h = .05, lty = 1)
   
-  plot(mu.test, power_nobs[4,], type = "l", xlab = bquote(theta), ylab = "Poder", main = "n = 100")
+  plot(mu.test, poder_nobs[4,], type = "l", xlab = bquote(theta), ylab = "Poder", main = "n = 100")
   abline(v = mu0, lty = 1)
   abline(h = .05, lty = 1)
